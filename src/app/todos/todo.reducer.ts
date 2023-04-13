e07c82af4041f0f5ec4ac803d9a96666c595ea40
Paula Campos <pkcamposc@falabella.cl>
2023-04-13 17:06:34 -0400

    feat: Definir AppState

--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,7 +2,9 @@ import { createReducer, on } from '@ngrx/store';
 import { crear } from './todo.actions';
 import { Todo } from './models/todo.model';
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [
+    new Todo('Salvar al mundo')
+];
 
 export const todoReducer = createReducer(
   initialState,
