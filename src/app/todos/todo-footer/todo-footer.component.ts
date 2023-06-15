302a4c119c7ec3444c75b0d90eff1d1c5d9b9d7c
Paula Campos <pkcamposc@falabella.cl>
2023-06-15 18:27:11 -0400

    Cambiar el filtro

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { setFilter, validFilters } from 'src/app/filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
   styleUrls: ['./todo-footer.component.scss']
 })
-export class TodoFooterComponent {
+export class TodoFooterComponent implements OnInit {
 
+  actualFilter: validFilters = 'todos';
+  filters: validFilters[] = ['todos', 'completados', 'pendientes'];
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+      this.store.select('filter').subscribe(filter => this.actualFilter = filter);
+  }
+
+  changeFilter(filter: validFilters) {
+    this.store.dispatch(setFilter({ filter }))
+  }
 }
