717c12cee958af8299640c0245960e6f0de1ffc5
Pau Campos <pau.campos89@gmail.com>
2023-12-20 17:01:34 -0300

    feat: conteo de tareas pendientes

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -2,24 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducer';
 import { setFilter, validFilters } from 'src/app/filter/filter.actions';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
-  styleUrls: ['./todo-footer.component.scss']
+  styleUrls: ['./todo-footer.component.scss'],
 })
 export class TodoFooterComponent implements OnInit {
-
   actualFilter: validFilters = 'todos';
   filters: validFilters[] = ['todos', 'completados', 'pendientes'];
+  pendingTasks: number = 0;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-      this.store.select('filter').subscribe(filter => this.actualFilter = filter);
+    this.store.subscribe((state) => {
+      this.actualFilter = state.filter;
+      this.pendingTasks = state.todos
+        .filter((todo: Todo) => !todo.completed).length;
+    });
   }
 
   changeFilter(filter: validFilters) {
-    this.store.dispatch(setFilter({ filter }))
+    this.store.dispatch(setFilter({ filter }));
   }
 }
